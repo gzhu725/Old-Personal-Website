9403ec74210d8bf30db4c41138cd1e4f9398225a
Gloria Zhu <85907267+gzhu725@users.noreply.github.com>
2022-12-05 14:58:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function sendEmail() {
     let message = document.getElementById("message").value;
     let finalmessage = `Name : ${name} <br>  Email : ${email} <br>  Message : ${message} <br>`;
     Email.send({
-    SecureToken : "99bafa80-ca79-45a9-8ba6-22ed71458b2a",
+    SecureToken : "4b5c58f5-8497-40d0-9ad5-d5ad5de50902",
     To : "gloriazhu66@gmail.com",
     From : "gloriazhu66@gmail.com",
     Subject: "MESSAGE FROM PERSONAL WEBSITE!",
